4558599220db1a761f7652aa8d6a1888e6f7bd48
Paisins <35646053+Paisins@users.noreply.github.com>
2018-04-16 16:33:30 +0800

    Create zhihu_xinyuanjieyi.py

---
+++ zhihu_xinyuanjieyi.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding:utf8 -*-
+import urllib.request
+import urllib.error
+from lxml import etree
+import os
+import os.path
+
+
+def geturlimg(url, path):
+    response = urllib.request.urlopen(url).read()
+    html = response.decode('utf-8')
+    page = etree.HTML(html)
+    imglist = page.xpath('//img/@src')
+    if not os.path.isdir(path):
+        os.makedirs(path)
+    i = 0
+    for img in imglist:
+        if extension(img):
+            filename = path + '//' + str(i) + extension(img)
+            try:
+                urllib.request.urlretrieve(img, filename)
+                i += 1
+            except urllib.error.URLError as e:
+                print('照片%d下载失败'%i + str(e))
+
+
+def extension(img):
+    ext = os.path.splitext(img)[1]
+    Type = ['.jpg', '.png', '.gif', '.jpeg']
+    if ext in Type:
+        return ext
+
+
+if __name__ == "__main__":
+    url = 'https://www.zhihu.com/question/22070147'
+    print('搜索的网址是%s' % url)
+    path = input('请输入存放的路径：')
+    geturlimg(url, path)
