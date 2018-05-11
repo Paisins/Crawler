d02ad33bc2abd2e758d9285996bdee34450bcaa3
Paisins <35646053+Paisins@users.noreply.github.com>
2018-05-11 12:41:37 +0800

    Add files via upload

--- Scrapy/fotomen/fotomen/items.py
+++ Scrapy/fotomen/fotomen/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class MovieItem(scrapy.Item):
+class FotomenItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    name = scrapy.Field()
+    keyword = scrapy.Field()
+    link = scrapy.Field()
+    desc = scrapy.Field()
