96a5c1e61cb735e73cdde8e15bc751b30491e600
Paisins <35646053+Paisins@users.noreply.github.com>
2018-05-11 12:42:17 +0800

    Add files via upload

--- Scrapy/fotomen/fotomen/pipelines.py
+++ Scrapy/fotomen/fotomen/pipelines.py
@@ -5,8 +5,15 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import requests
+import os
 
-class MoviePipeline(object):
+class FotomenPipeline(object):
     def process_item(self, item, spider):
-        with open('C:\\Users\\dell\\Desktop\\test.txt', 'a' ) as f:
-            f.write(item['name'] + '\n')
+        filepath = 'D:\\My Python\\爬虫\\爬虫数据\\fotomen_scrapy\\'+ item['keyword']
+        if not os.path.isdir(filepath):
+            os.mkdir(filepath)
+        filename = filepath + '\\' + item['desc']
+        response = requests.get(item['link'])
+        with open(filename+'.jpg', 'wb') as f:
+            f.write(response.content)
